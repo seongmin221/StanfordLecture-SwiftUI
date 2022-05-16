5a29cf905603609173d44a2d1f75109eacba9eca
seongmin221 <seongmin221@naver.com>
2022-05-17 08:52:48 +0900

    lecture1: in progress

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 이성민 on 2022/05/16.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
