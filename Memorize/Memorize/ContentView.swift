2ab7488f9af7c2d9c4abd81c7a81d7b4a44b5278
seongmin221 <seongmin221@naver.com>
2022-05-18 17:32:29 +0900

    lecture2: in progress

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,18 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack(content: {
-            RoundedRectangle(cornerRadius: 25)
-                .stroke(lineWidth: 3)
-            Text("")
-        })
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }
         .padding(.horizontal)
         .foregroundColor(.red)
     }
 }
 
+struct CardView: View {
+    var body: some View {
+        ZStack(content: {
+            RoundedRectangle(cornerRadius: 25)
+                .stroke(lineWidth: 3)
+            Text("✈️")
+        })
+    }
+}
+
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+            
     }
 }
